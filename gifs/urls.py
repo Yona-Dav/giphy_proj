67faaa74929a705187de7c2f4643f1432b5e5000
Yona-Dav <yona.bohbot@gmail.com>
2021-11-24 11:00:32 +0200

    add navbar and like/dislike

--- gifs/urls.py
+++ gifs/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('homepage/', views.homepage, name='all_gifs'),
     path('category/<int:category_id>/', views.category, name='single_category'),
     path('category/all/', views.all_categories, name='all_categories'),
-    path('homepage/<int:gif_id>/', views.single_gif, name='single_gif')
+    path('homepage/<int:gif_id>/', views.single_gif, name='single_gif'),
+    path('homepage/<int:gif_id>/<int:liked>/', views.gif_like_action, name='like_link')
 ]
--- gifs/views.py
+++ gifs/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Gif, Category
+
 # Create your views here.
 
 def homepage(request):
-    gifs = Gif.objects.all()
-    return render(request, 'homepage.html', {'all_gifs': gifs})
+    return render(request, 'homepage.html', {'all_gifs': Gif.objects.all()})
 
 def category(request, category_id):
     cat = Category.objects.get(id=category_id)
@@ -12,9 +12,17 @@ def category(request, category_id):
     return render(request, 'category.html', {'gifs': gifs, 'category':cat})
 
 def all_categories(request):
-    categories = Category.objects.all()
-    return render(request, 'categories.html', {'all_categories': categories})
+    return render(request, 'categories.html', {'all_categories': Category.objects.all()})
 
 def single_gif(request, gif_id):
     gif = Gif.objects.get(id=gif_id)
     return render(request, 'gif.html', {'gif': gif})
+
+def gif_like_action(request, gif_id, liked):
+    gif = Gif.objects.get(id=gif_id)
+    if liked:
+        gif.likes += 1
+    else:
+        gif.likes -= 1
+    gif.save()
+    return redirect('single_gif', gif.id)
